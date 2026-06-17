ea3e7b519f01da8029162644a72fea0ff0fff89d
prab-hector <prabedu77@gmail.com>
2026-06-17 15:54:58 +0530

    storage, models, created

---
+++ storage/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+class Storage(models.Model):
+    name = models.CharField(max_length=30)
+    branch = models.CharField(max_length=30)
+    division = models.CharField(max_length=20)
+    current_project = models.CharField(max_length=30)
+    domain = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=10, null=False, blank=False)
+    year = models.CharField(max_length=30)
+    about = models.TextField(default="No bio or description provided yet.", blank=True)
+    rfid_num = models.CharField(max_length=8)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
