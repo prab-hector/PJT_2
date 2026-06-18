7e0eabf335dfadcb81b6cefcc0b7039a636858bf
prab-hector <prabedu77@gmail.com>
2026-06-18 14:09:40 +0530

    server stopped woking again ne model added, in datacommi g

--- rfid_datacoming/models.py
+++ rfid_datacoming/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class RFIDLog(models.Model):
+    uid = models.CharField(max_length=50)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"UID: {self.uid} at {self.timestamp}"
